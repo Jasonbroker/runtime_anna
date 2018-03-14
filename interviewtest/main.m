386e6a97b68bcc52af778d9967c21bd23b333ef9
Jasonbroker <zhiye1@hotmail.com>
2018-03-14 13:52:11 +0800

    Initial Commit

---
+++ interviewtest/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  interviewtest
+//
+//  Created by Jason on 14/03/2018.
+//  Copyright © 2018 bmcode. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
