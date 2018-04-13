a1caee8e7f46c6116b1d10af1bcbf03494396ff1
Jasonbroker <zhiye1@hotmail.com>
2018-04-13 16:36:56 +0800

    fix

--- interviewtest/main.m
+++ interviewtest/main.m
@@ -8,10 +8,43 @@
 
 #import <Foundation/Foundation.h>
 
+@interface XXOB : NSObject
+
++ (void)hello;
+- (void)hello;
+
+@end
+
+@implementation XXOB
+- (void)hello {
+    
+}
+
+@end
+
+void test1(void);
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
-        NSLog(@"Hello, World!");
+        
+        @autoreleasepool {
+            test1();
+        }
+        
+        XXOB *obj;
+//        obj= ()0x100001188;
+        [obj hello];
+        do {
+            
+        } while (1);
     }
     return 0;
+}
+
+void test1() {
+    NSLog(@"%p", [XXOB class]);
+    XXOB *obj = [XXOB new];
+    [obj hello];
+    [obj addObserver:obj forKeyPath:@"hellp" options:NSKeyValueObservingOptionNew context:&obj];
 }
